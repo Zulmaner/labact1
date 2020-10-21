23b971abd72e30f05db5a55b2d7a951183661e18
Eizul Aiman <2018272976@isiswa.uitm.edu.my>
2020-10-20 19:01:20 -0700

    first commit

---
+++ helloworld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void){
+printf("HelloWorld!\n");
+printf("I Love C\n");
+return(0);
+}
---
+++ userinput.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+int age;
+printf("Hallo, How old are yoou ? > ");
+scanf("%d", &age);
+printf("you are %d years old ",age);
+return(0);
+}
